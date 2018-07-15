5f5b4a161867680e778397695118e8a1a5ec39cc
Cuda Chen <clh960524@gmail.com>
2018-07-15 11:57:31 +0800

    [add] add inverse wavelet transform feature

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
 	int height = img.rows;
 
 	
-	int level = 3; // level of decomposition (restruction)
+	int level = 1; // level of decomposition (restruction)
 	int levelCount = 1;
 	const float divisor = 2;
 	Mat tmp = Mat::ones(width, height, CV_32FC1);
@@ -20,6 +20,7 @@ int main()
 	Mat imgtmp = img.clone();
 	imgtmp.convertTo(imgtmp, CV_32FC1);
 
+	// do wavelet transformation
 	while(levelCount <= level)
 	{
 		//width /= 2;
@@ -57,6 +58,57 @@ int main()
 
 	wavelet.convertTo(wavelet, CV_8UC1);
 	imshow("wavelet", wavelet);
+	waitKey(0);
+
+	// reset the variables I will use later
+	width = img.cols;
+	height = img.rows;
+	levelCount = 1;
+	tmp = Mat::ones(width, height, CV_32FC1);
+	wavelet.convertTo(wavelet, CV_32FC1);
+	imgtmp = wavelet.clone();
+	wavelet = Mat::ones(width, height, CV_32FC1);
+
+	cout << "before resize: " << height << " "  << width << endl;
+	height = height / pow(2, level);
+	width = width / pow(2, level);
+	cout << "after resize: " << height << " " << width << endl;
+
+	// do inverse wavelet transformatin
+	while(levelCount <= level)
+	{
+		for(int i = 0; i < height; i++)
+		{
+			for(int j = 0; j < width; j++)
+			{
+				tmp.at<float>(2 * i, j) = imgtmp.at<float>(i, j) + imgtmp.at<float>(i + height, j);
+				tmp.at<float>(2 * i + 1, j) = imgtmp.at<float>(i, j) - imgtmp.at<float>(i + height, j);
+			}
+		}
+		imshow("vertical inverse", tmp);
+		waitKey(0);
+		height *= 2;
+
+		for(int i = 0; i < height; i++)
+		{
+			for(int j = 0; j < width; j++)
+			{
+				wavelet.at<float>(i, 2 * j) = tmp.at<float>(i, j) + tmp.at<float>(i, j + width);
+				wavelet.at<float>(i, 2 * j + 1) = tmp.at<float>(i, j) - tmp.at<float>(i, j + width);
+			}
+		}
+		imshow("horizontal inverse", wavelet);
+		waitKey(0);
+		width *= 2;
+
+		imgtmp = wavelet;
+		/*width *= 2;
+		height *= 2;*/
+		levelCount++;
+	}	
+
+	wavelet.convertTo(wavelet, CV_8UC1);
+	imshow("reconstruct", wavelet);
 	waitKey(0);
 
 	return 0;
